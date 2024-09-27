16e3078ac912ec01f992efc02950d19f037ecbbd
Sohntech <ndiagalo259@gmail.com>
2024-09-27 07:03:38 +0000

    nginx fixing

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Démarre PHP-FPM en arrière-plan
-php-fpm &
-
-# Attendre un peu que PHP-FPM soit prêt
-
 # Démarre Nginx
 service nginx start
 
-# Garder le script en cours d'exécution
+# Démarre PHP-FPM
+php-fpm
